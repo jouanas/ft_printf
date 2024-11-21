1f9ac9f9f87e060f05c5e43149039769b481655c
Salma Jouan <sjouan@e1r12p9.1337.ma>
2024-11-21 13:46:16 +0100

    add

--- func.c
+++ func.c
@@ -10,4 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putchar(char c, int fd)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s && s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n *= -1;
+	}
+	if (n < 10)
+	{
+		c = n + 48;
+		write(1, &c, 1);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10);
+		ft_putnbr_fd(n % 10);
+	}
+}
--- printf.h
+++ printf.h
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include "libft.h"
 
 
 
